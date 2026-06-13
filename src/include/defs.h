6f3cc3835c9641829a28038960427aff1a4e86b7
marcos727 <marco.enrique.garrido@gmail.com>
2026-06-13 14:44:50 -0300

    arreglar errores de compilacion

--- src/include/defs.h
+++ src/include/defs.h
@@ -7,7 +7,8 @@
 #define TICKS_FOR_NEXT_FRAME (1000 / 60)
 #define FONT_SIZE 120
 //cambiar defines por tipo de datos
-string ASSET_BG = "./assets/img/bg.png";
+
+#define ASSET_BG "./assets/img/bg.png"
 #define ASSET_BALL "./assets/img/ball.png"
 #define ASSET_BAR "./assets/img/bar.png"
 #define ASSET_WALL "./assets/img/wall.png"
