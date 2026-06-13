6f8fa5c9414394586233655cdd31cc92235a6fd5
marcos727 <marco.enrique.garrido@gmail.com>
2026-06-13 18:06:31 -0300

    copy fix

--- copy.ps1
+++ copy.ps1
@@ -2,5 +2,5 @@ robocopy "libs\SDL2\lib\x64" "out" "*.dll"
 robocopy "libs\SDL2_image\lib\x64" "out" "*.dll"
 robocopy "libs\SDL2_ttf\lib\x64" "out" "*.dll"
 robocopy "libs\SDL2_mixer\lib\x64" "out" "*.dll"
-robocopy "assets" "out" /e
+robocopy "assets" "out\assets" /e
 exit 0
